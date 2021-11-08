ad9e241a6ac233040322ffdfb94b0b03f5d84c5f
Joe <joe@coder.com>
2021-11-08 17:49:45 +0000

    wip

--- tests/security_context_test.go
+++ tests/security_context_test.go
@@ -77,3 +77,60 @@ func TestSecurityContext(t *testing.T) {
 		})
 	}
 }
+
+func TestPostgresValues(t *testing.T) {
+	t.Parallel()
+
+	chart, err := loader.LoadDir("..")
+	require.NoError(t, err, "loaded chart successfully")
+	exampleOpenShift, err := ReadValues("../examples/openshift/openshift.values.yaml")
+	require.NoError(t, err, "failed to OpenShift example values")
+
+	tests := []struct {
+		Name     string
+		Values   *CoderValues
+		Postgres *corev1.Postgres
+	}{
+		{
+			Name:   "openshift",
+			Values: exampleOpenShift,
+			Postgres: &any{
+				Default: &any{
+					Resources: &any{
+						Requests: &any{
+							// Expect this to fail
+							CPU:    "1m",
+							Memory: "33Mi",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+
+			objs, err := RenderChart(chart, test.Values, nil, nil)
+			require.NoError(t, err, "failed to render chart")
+
+			// Find the coderd Deployment
+			var found bool
+			for _, obj := range objs {
+				deployment, ok := obj.(*appsv1.Deployment)
+				if ok && deployment.Name == "coderd" {
+					found = true
+
+					expected := test.Postgres
+					// TODO@jsjoeio not sure what to do here.
+					actual := deployment.Spec.Template.Spec.SecurityContext
+					require.Equal(t, expected, actual, "expected matching PodSecurityContext")
+					break
+				}
+			}
+			require.True(t, found, "expected coderd deployment in manifests")
+		})
+	}
+}
