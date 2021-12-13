95c0cffb4e52bdbe4f32885020bad2a5135f185c
Colin Adler <colin@coder.com>
2021-12-13 14:11:15 -0600

    chore: fuzz testing

---
+++ tests/fuzz_test.go
@@ -0,0 +1,63 @@
+package tests
+
+import (
+	"math/rand"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/mitchellh/reflectwalk"
+	"github.com/pioz/faker"
+)
+
+func TestFuzz(t *testing.T) {
+	t.Parallel()
+
+	seed := time.Now().UnixNano()
+	rand.Seed(seed)
+	t.Log("seed:", seed)
+
+	c := &CoderValues{}
+	faker.Build(c)
+	reflectwalk.Walk(c, walker{rand: rand.Float64()})
+
+	LoadChart(t).MustRender(t, func(cv *CoderValues) { *cv = *c })
+}
+
+var _ reflectwalk.PrimitiveWalker = walker{}
+var _ reflectwalk.MapWalker = walker{}
+var _ reflectwalk.SliceWalker = walker{}
+var _ reflectwalk.ArrayWalker = walker{}
+
+// walker randomly unassigns values based on the specified random chance.
+type walker struct {
+	rand float64
+}
+
+func (w walker) Primitive(v reflect.Value) error {
+	w.processValue(v)
+	return nil
+}
+
+func (w walker) MapElem(_, _, _ reflect.Value) error { return nil }
+func (w walker) Map(v reflect.Value) error {
+	w.processValue(v)
+	return nil
+}
+
+func (w walker) SliceElem(_ int, _ reflect.Value) error { return nil }
+func (w walker) Slice(v reflect.Value) error {
+	w.processValue(v)
+	return nil
+}
+func (w walker) ArrayElem(_ int, _ reflect.Value) error { return nil }
+func (w walker) Array(v reflect.Value) error {
+	w.processValue(v)
+	return nil
+}
+
+func (w walker) processValue(v reflect.Value) {
+	if v.CanSet() && rand.Float64() < w.rand {
+		v.Set(reflect.Zero(v.Type()))
+	}
+}
--- tests/go.mod
+++ tests/go.mod
@@ -4,6 +4,8 @@ go 1.17
 
 require (
 	github.com/jinzhu/copier v0.3.4
+	github.com/mitchellh/reflectwalk v1.0.2
+	github.com/pioz/faker v1.7.2
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	helm.sh/helm/v3 v3.7.1
@@ -31,7 +33,6 @@ require (
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/mitchellh/copystructure v1.1.1 // indirect
-	github.com/mitchellh/reflectwalk v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
